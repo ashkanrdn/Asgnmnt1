d3bbfeedb0fd8c050645196f905101da35f53b2f
Ashkan Radnia <ashkan.rdn@gmail.com>
2020-09-28 11:12:44 -0400

    server

--- server.js
+++ server.js
@@ -1,16 +1,48 @@
 const http = require('http');
-const fs = require('fs')
+const fs = require('fs');
+const { RSA_NO_PADDING } = require('constants');
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method);
+
+    //Set Header
     res.setHeader('Content-Type', 'text/plain');
 
-    fs.readFile('./index.html', (err, data) => {
+    //Define Path and Switch
+
+
+    let path = './Views';
+    switch (req.url) {
+        case '/':
+            path += '/index.html';
+            res.statusCode = 200;
+            break;
+        case '/connections':
+            path += '/connections1.html';
+            res.statusCode = 200;
+            break;
+        case '/detail':
+            path += '/ConnectionDetails.html';
+            res.statusCode = 200;
+            break;
+        default:
+            path += '/savedconnections.html';
+            res.statusCode = 404;
+            break;
+
+    }
+
+
+
+    fs.readFile(path, (err, data) => {
         if (err) {
             console.log(err);
+
             res.end();
 
         } else {
+            res.end(data);
+
 
 
         }
