eea6cc2a2c5f41b1695f4be1013e2d4302e249e4
ashkanrdn <41102393+ashkanrdn@users.noreply.github.com>
2020-11-22 11:42:32 -0500

    first

--- app.js
+++ app.js
@@ -1,55 +1,99 @@
 //express app 
 const express = require('express');
+const { getConnections, connectionsList, getConnection } = require('./models/connections');
 const app = express();
 app.use('/public', express.static('public'));
 app.set('view engine', 'ejs');
 
 // listen for requests 
 
-app.listen(8084);
+app.listen(8000);
 
 
 
 
 app.get('/', (req, res) => {
 
-    res.render('index');
+    res.render('index', { title: "Home" });
 
 
 });
+
 app.get('/index', (req, res) => {
 
     res.redirect('/');
 
 
 });
+
+
 app.get('/connections', (req, res) => {
 
-    res.render('connections');
+    let conListHandler = getConnections();
+
+    res.render('connections', { conListHandler, title: "Connections" });
 
 
 });
+
+
+
+app.get('/connection/:name', (req, res, next) => {
+
+    const paramName = req.params.name;
+
+    let eventhandle = getConnection(paramName);
+    console.log(eventhandle);
+
+
+
+
+
+    if (eventhandle === 'undefined') {
+        res.status(404)
+        res.render('error', { title: "404 error" });
+
+
+    } else {
+
+        let eventhandle = getConnection(paramName);
+
+        res.render('connection', { eventhandle, title: " Details of " + eventhandle.conName });
+
+
+    }
+
+
+
+});
+
+
 app.get('/connection', (req, res) => {
+    res.redirect('/connections');
+})
 
-    res.render('connection');
 
-
-});
 app.get('/SavedConnections', (req, res) => {
 
-    res.render('SavedConnections');
+    res.render('SavedConnections', { title: "Saved connections" });
 
 
 });
 app.get('/contact', (req, res) => {
 
-    res.render('contact');
+    res.render('contact', { title: "Contact" });
 
 
 });
 app.get('/about', (req, res) => {
 
-    res.render('about');
+    res.render('about', { title: "About us" });
 
 
 });
+
+
+app.use((req, res) => {
+    res.render('error', { title: "404 error" });
+
+})
--- models/connections.js
+++ models/connections.js
@@ -1,30 +1,35 @@
 const connectionsList = [
-    { conId: 'AA11', conName: "NameHandle", conTopic: "Topic", condDetails: "Details", conDate: "date", conHost: "HostName", conImage: "Path" },
-    { conId: 'AA12', conName: "NameHandle", conTopic: "Topic", condDetails: "Details", conDate: "date", conHost: "HostName", conImage: "Path" },
-    { conId: 'AA13', conName: "NameHandle", conTopic: "Topic", condDetails: "Details", conDate: "date", conHost: "HostName", conImage: "Path" },
-    { conId: 'AA14', conName: "NameHandle", conTopic: "Topic", condDetails: "Details", conDate: "date", conHost: "HostName", conImage: "Path" },
+
+
+
+
+    { conId: 'AA12', conHost: "Elaine Hess", conTopic: "Lecture", condDetails: "Just becaouse Halloween lo", conTime: "16:30 -18:30", conDate: "10/22/2020", conName: "Art Hype", conLoc: "StudentUnion Room A", conImage: "/public/img/NameHandle1.jpg" },
+    { conId: 'AA12', conHost: "Bayley Yang", conTopic: "Lecture", condDetails: "Just because Halloween loo", conTime: "15:30 -18:30", conDate: "10/22/2020", conName: "Art History", conLoc: "StudentUnion Room B", conImage: "/public/img/NameHandle2.jpg" },
+    { conId: 'AA13', conHost: "Baran Hollis", conTopic: "Lecture", condDetails: "Just because Halloween loo", conTime: "14:30 -16:30", conDate: "10/22/2020", conName: "Design Market", conLoc: "StudentUnion Room C", conImage: "/public/img/NameHandle3.jpg" },
+    { conId: 'AA13', conHost: "Eve Cannon", conTopic: "Workshop", condDetails: "Just because Halloween loo", conTime: "8:30 -10:30", conDate: "10/22/2020", conName: "Intro Art", conLoc: "StudentUnion Room D", conImage: "/public/img/NameHandle4.jpg" },
+    { conId: 'AA13', conHost: "Tere llivan", conTopic: "Workshop", condDetails: "Just because Halloween loo", conTime: "9:30 -11:30", conDate: "10/22/2020", conName: "Intro Graphics", conLoc: "StudentUnion Room E", conImage: "/public/img/NameHandle5.jpg" },
+    { conId: 'AA14', conHost: "Ellen Bush", conTopic: "Workshop", condDetails: "Just because Halloween loo", conTime: "10:30 -12:30", conDate: "10/22/2020", conName: "Hands on photography", conLoc: "StudentUnion Room F", conImage: "/public/img/NameHandle6.jpg" },
 ]
 
 
-function getConnections(dataBase) {
+function getConnections() {
 
-    return dataBase;
+    return connectionsList;
 
 }
 
 function getConnection(IDofConnection) {
 
-    let connectionTemp = connectionsList.find(item => item.conId === IDofConnection);
+    let connectionTemp = connectionsList.find(item => item.conName === IDofConnection);
     if (connectionTemp == undefined) {
         return 'undefined';
     } else {
 
-        console.log(connectionTemp);
+
         return connectionTemp;
     }
 
 }
-
-
 module.exports.getConnections = getConnections;
 module.exports.getConnection = getConnection;
+module.exports.connectionsList = connectionsList;
